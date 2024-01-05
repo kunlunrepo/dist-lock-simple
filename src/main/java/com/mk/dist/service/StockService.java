916ea5eebf36a10f0f4bb579630f9befbeafa900
kunlunrepo <482976531@qq.com>
2024-01-05 10:29:53 +0800

    jvm锁失效场景：service多例模式

--- src/main/java/com/mk/dist/service/StockService.java
+++ src/main/java/com/mk/dist/service/StockService.java
@@ -2,6 +2,8 @@ package com.mk.dist.service;
 
 import com.mk.dist.dao.StockDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,38 +11,31 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * description :
  *
- * @author zhoujian
+ * @author kunlunrepo
  * date :  2024-01-04 17:16
  */
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Service
 public class StockService {
 
     @Autowired
     private StockDao stockDao;
 
-    ReentrantLock lock = new ReentrantLock();
-    public String deductStock(Long goodId, Integer count) {
-        // 上锁
-        lock.lock();
+    public synchronized String deductStock(Long goodId, Integer count) {
+        // 1.查询商品的库存数量
+        Integer stock = stockDao.selectStockByGoodsId(goodId);
 
-        try {
-            // 1.查询商品的库存数量
-            Integer stock = stockDao.selectStockByGoodsId(goodId);
+        // 2.判断库存数量是否足够
+        if (stock < count) {
+            return "库存不足";
+        }
 
-            // 2.判断库存数量是否足够
-            if (stock < count) {
-                return "库存不足";
-            }
-
-            // 3.如果库存足够，则扣减库存数量，并返回成功信息
-            Integer flag = stockDao.updateStockByGoodsId(goodId, stock - count);
-            if (flag == 1) {
-                return "扣减成功";
-            } else {
-                return "扣减失败";
-            }
-        } finally {
-            lock.unlock();
+        // 3.如果库存足够，则扣减库存数量，并返回成功信息
+        Integer flag = stockDao.updateStockByGoodsId(goodId, stock - count);
+        if (flag == 1) {
+            return "扣减成功";
+        } else {
+            return "扣减失败";
         }
     }
 }
