612aece422cbb6aae0b4caa0be2a842e5a09272d
kunlunrepo <482976531@qq.com>
2024-01-05 10:22:57 +0800

    ReentrantLock 解决超卖

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+# 性能
+
 **#无锁**
 
 ![image-20240105093630574](readme.assets/image-20240105093630574.png)
@@ -6,3 +8,6 @@
 
 ![image-20240105093820653](readme.assets/image-20240105093820653.png)
 
+**#ReentrantLock**
+
+![image-20240105101512939](readme.assets/image-20240105101512939.png)
--- src/main/java/com/mk/dist/service/StockService.java
+++ src/main/java/com/mk/dist/service/StockService.java
@@ -4,6 +4,8 @@ import com.mk.dist.dao.StockDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * description :
  *
@@ -16,21 +18,29 @@ public class StockService {
     @Autowired
     private StockDao stockDao;
 
-    public synchronized String deductStock(Long goodId, Integer count) {
-        // 1.查询商品的库存数量
-        Integer stock = stockDao.selectStockByGoodsId(goodId);
+    ReentrantLock lock = new ReentrantLock();
+    public String deductStock(Long goodId, Integer count) {
+        // 上锁
+        lock.lock();
 
-        // 2.判断库存数量是否足够
-        if (stock < count) {
-            return "库存不足";
-        }
+        try {
+            // 1.查询商品的库存数量
+            Integer stock = stockDao.selectStockByGoodsId(goodId);
 
-        // 3.如果库存足够，则扣减库存数量，并返回成功信息
-        Integer flag = stockDao.updateStockByGoodsId(goodId, stock - count);
-        if (flag == 1) {
-            return "扣减成功";
-        } else {
-            return "扣减失败";
+            // 2.判断库存数量是否足够
+            if (stock < count) {
+                return "库存不足";
+            }
+
+            // 3.如果库存足够，则扣减库存数量，并返回成功信息
+            Integer flag = stockDao.updateStockByGoodsId(goodId, stock - count);
+            if (flag == 1) {
+                return "扣减成功";
+            } else {
+                return "扣减失败";
+            }
+        } finally {
+            lock.unlock();
         }
     }
 }
